59790cabf79132a26f064b60b7dfc42896f6e3b3
mirzayogy <nurulrachmawati70@gmail.com>
2021-07-22 19:50:52 +0800

    Nurul_UAS_WEB2

---
+++ app/Nilai.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nilai extends Model
+{
+    
+
+    public function mahasiswa()
+    {
+
+    }
+    public function makul()
+    {
+        
+    }
+}
